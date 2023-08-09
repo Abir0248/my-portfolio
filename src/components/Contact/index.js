50e93151231cd28ec8d40797a5913b322ad0b886
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-09 07:21:26 +0600

    contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,131 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_nv7vvdh',
+        'template_6jkesyz',
+        form.current,
+        'Ie-QEcBtuFgPzJw8f'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I thrive in challenging environments and am always eager to embark
+            on ambitious or expansive projects. Web Development has been a
+            passion of mine, and I'm continuously excited by the unique
+            challenges and experiences it brings.
+          </p>
+          <p>
+            That being said, I'm open to all sorts of inquiries and proposals.
+            Whether you have a project in mind or simply a question, I'd love to
+            hear from you. Feel free to get in touch using the form below.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    placeholder="Name"
+                    type="text"
+                    name="user_name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="user_email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Abir Hasnat,
+          <br />
+          56 Jagannath Saharoad, <br />
+          Lalbagh, Dhaka 1211 <br />
+          Bangladesh,
+          <br />
+          +8801772030248
+          <br />
+          <span>abirhasnat00@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer
+            center={[23.71972247901826, 90.38217856615212]}
+            zoom={13}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[23.7209668195878, 90.37994877216562]}>
+              <Popup>Abir lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
