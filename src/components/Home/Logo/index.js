a3d794e67d1a3441c7e372ba14b3d7aa72fa40ae
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-09 18:48:08 +0600

    error fix

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,27 +1,17 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
 
 const Logo = () => {
   const bgRef = useRef()
-  const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 7,
-      })
+    gsap.timeline().to(bgRef.current, {
+      duration: 1,
+      opacity: 1,
+    })
 
     gsap.fromTo(
       solidLogoRef.current,
