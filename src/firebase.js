2f741e51f533e7b4ada33be74e4bbed2f581c15f
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-09 10:58:28 +0600

    dashboard

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore/lite'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIeQxXci4kKTecpOq17zhIbO0dAwLyFbY',
+  authDomain: 'react-portfolio-dashboar-6440f.firebaseapp.com',
+  projectId: 'react-portfolio-dashboar-6440f',
+  storageBucket: 'react-portfolio-dashboar-6440f.appspot.com',
+  messagingSenderId: '93974301263',
+  appId: '1:93974301263:web:63bff9796b434a0dfbf83a',
+  measurementId: 'G-H3XJ5118C4',
+}
+
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth()
+const provider = new GoogleAuthProvider()
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider)
